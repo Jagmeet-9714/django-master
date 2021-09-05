2b0f582e3bf5f47019f50a4d22eff329494fd369
jagmeet singh <cr2436812@gmail.com>
2021-09-06 00:22:04 +0530

    added allowed host

--- Django1/settings.py
+++ Django1/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
