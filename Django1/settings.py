f6a6e1e1c0ec15a5efab521028b2756ccb5c90ad
jagmeet singh <cr2436812@gmail.com>
2021-09-05 23:58:34 +0530

    first commit

--- Django1/settings.py
+++ Django1/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -128,6 +128,6 @@ STATIC_URL = '/static/'
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #added manually
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
